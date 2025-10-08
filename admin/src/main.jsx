1552763735ead9493b431d78004e4f897e722177
Nishant Sanjay borude <nishantborude555@gmail.com>
2025-10-08 15:53:48 +0530

    Refactor context providers in main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,18 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import AuthContext from './context/AuthContext'
-import AdminContext from './context/AdminContext'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './context/AuthContext';
+import AdminProvider from './context/AdminContext';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <AuthContext>
-    <AdminContext>
-        <App/>
-    </AdminContext>
-  </AuthContext>
+    <AuthProvider>
+      <AdminProvider>
+        <App />
+      </AdminProvider>
+    </AuthProvider>
   </BrowserRouter>
-)
- 
+);
