b7b490b85efac3b474ea7c3ec787c13de0ada529
Nishant Sanjay borude <nishantborude555@gmail.com>
2025-10-08 16:07:03 +0530

    Update main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import  AdminProvider from './Context/AdminContext';
+import AdminProvider from './Context/AdminContext';
 import AuthProvider from './Context/AuthContext';
-
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
