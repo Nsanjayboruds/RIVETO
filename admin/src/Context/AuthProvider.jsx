a4388a715d8ef599b5f3acd5f70af8a73e717b58
Nishant Sanjay borude <nishantborude555@gmail.com>
2025-10-08 17:15:57 +0530

    Update AuthProvider.jsx

--- admin/src/Context/AuthProvider.jsx
+++ admin/src/Context/AuthProvider.jsx
@@ -4,7 +4,7 @@ import React, { createContext } from 'react';
 export const authDataContext = createContext();
 
 function AuthProvider({ children }) {
-  const serverUrl = "https://riveto-backend.onrender.com";
+  const serverUrl = "https://riveto-backend.onrender.com"; 
 
   const value = { serverUrl };
 
